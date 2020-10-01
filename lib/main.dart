63526cb8f6cc3749605bf433d5ec0376184fbda2
diego dussan <dedussan@corhuila.edu.co>
2020-10-01 12:14:00 -0500

    implementar el splash al cargar la aplicacion luego muestra el seleccion de instituciones

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_csm_tecnologia/src/pages/home-page.dart';
 import 'package:flutter_csm_tecnologia/src/pages/institition-page.dart';
 import 'package:flutter_csm_tecnologia/src/pages/login_page.dart';
+import 'package:flutter_csm_tecnologia/src/pages/splash_screen.dart';
 import 'package:flutter_csm_tecnologia/theme/theme.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: miTema,
       title: 'Csm Educativo App',
-      initialRoute: 'home',
+      initialRoute: 'splash',
       routes: {
+        'splash': (context) => SplashScreen(),
         'home': (context) => HomePage(),
         'institution': (context) => InstPage(),
         'login': (context) => LoginPage(),
---
+++ lib/src/pages/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_csm_tecnologia/src/pages/home-page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 5),
+      () => Navigator.of(context).pushReplacementNamed('home'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _imgSplash(context),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Container(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.black45,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _imgSplash(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
+
+    final _imgFondo = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          child: Container(
+            height: _size.height * 1,
+            width: _size.width * 1,
+            child: Image(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/drawable-fhd/ic_action_splash.png'),
+            ),
+          ),
+        )
+      ],
+    );
+    return _imgFondo;
+  }
+}
